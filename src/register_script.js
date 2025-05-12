5f3e995465474d4425c374108bf1844b286d0983
Joana Carvalhal <joana.d.carvalhal@gmail.com>
2025-05-12 22:54:27 +0100

    register animations

---
+++ src/register_script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('container');
+const overlayBtn = document.getElementById('overlayBtn');
+
+overlayBtn.addEventListener('click', () => {
+    container.classList.toggle('right-panel-active');
+
+    overlayBtn.textContent = container.classList.contains('right-panel-active')
+        ? 'Register as Tutor'
+        : 'Register as Student';
+
+    overlayBtn.style.color =  container.classList.contains('right-panel-active')
+        ? '#32533D'
+        : '#03254E';
+});
